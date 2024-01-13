3e6fb386720158923511a16489b777099cb40382
François Rullière <kapochamo@gmail.com>
2024-01-14 00:23:29 +0800

    reverse

--- src/lib/render.js
+++ src/lib/render.js
@@ -25,6 +25,9 @@ Handlebars.registerHelper('parse', function (arg1, options) {
 Handlebars.registerHelper('ifEq', function (arg1, arg2, options) {
   return arg1 === arg2 ? options.fn(this) : options.inverse(this)
 })
+Handlebars.registerHelper('ifNeg', function (arg1,  options) {
+  return arg1 < 0 ? options.fn(this) : options.inverse(this)
+})
 Handlebars.registerHelper('default', function (arg1, arg2, options) {
   return arg1 || arg2
 })
