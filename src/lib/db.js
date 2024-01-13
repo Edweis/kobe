a60d6cd7ffdd3316adc4a35037d27045a8736767
François Rullière <kapochamo@gmail.com>
2024-01-13 18:06:32 +0800

    move db

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,7 +2,7 @@ import { open } from 'sqlite';
 import sqlite3 from 'sqlite3';
 
 sqlite3.verbose();
-const DATABASE_FILE = './src/database.db';
+const DATABASE_FILE = './database.db';
 
 
 // init database
