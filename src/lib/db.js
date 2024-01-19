492bea03097ecae4fbb7700d2644c633f4e184bf
François Rullière <kapochamo@gmail.com>
2024-01-19 10:20:38 +0800

    fixed

--- src/lib/db.js
+++ src/lib/db.js
@@ -58,6 +58,18 @@ if(!IS_PROD){
   INSERT INTO lines (id, created_at, name, amount, currency, paid, project_id, split)
   VALUES ('lin_4', '2024-01-01T00:43', 'Weekend camille', 170730, 'IDR', 'kaille', 'pro_123', '[{"participant": "francois", "amount": 85365}, {"participant": "kaille", "amount": 85365}]')
   ON CONFLICT (id, project_id) DO NOTHING;
+
+  INSERT INTO lines (id, created_at, name, amount, currency, paid, project_id, split)
+  VALUES ('lin_5', '2024-01-01T00:43', 'Weekend camille', 170730, 'IDR', 'kaille', 'pro_123', '[{"participant": "francois", "amount": 85365}, {"participant": "kaille", "amount": 85365}]')
+  ON CONFLICT (id, project_id) DO NOTHING;
+
+  INSERT INTO lines (id, created_at, name, amount, currency, paid, project_id, split)
+  VALUES ('lin_6', '2024-01-01T00:43', 'Weekend camille', 170730, 'IDR', 'kaille', 'pro_123', '[{"participant": "francois", "amount": 85365}, {"participant": "kaille", "amount": 85365}]')
+  ON CONFLICT (id, project_id) DO NOTHING;
+
+  INSERT INTO lines (id, created_at, name, amount, currency, paid, project_id, split)
+  VALUES ('lin_7', '2024-01-01T00:43', 'Weekend camille', 170730, 'IDR', 'kaille', 'pro_123', '[{"participant": "francois", "amount": 85365}, {"participant": "kaille", "amount": 85365}]')
+  ON CONFLICT (id, project_id) DO NOTHING;
   `)
 }
 
